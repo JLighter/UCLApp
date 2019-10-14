0dbac7bd48f6238771becccbb28409e5bb90709c
Formateur UCL <formateur-ucl@OASIS.UCLOUVAIN.BE>
2019-10-14 15:08:02 +0200

    Interpolation

--- src/app/vinyl/components/vinyl-preview/vinyl-preview.component.ts
+++ src/app/vinyl/components/vinyl-preview/vinyl-preview.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VinylPreviewComponent implements OnInit {
 
+  vinyl = {
+    title: 'Uprising',
+    artist: 'Bob Marley',
+    releaseDate: 1980,
+    imageUrl: 'https://static.fnac-static.com/multimedia/Images/FR/NR/4c/d7/12/1234764/1540-1/tsp20161011114552/Uprising.jpg'
+  }
+
   constructor() { }
 
   ngOnInit() {
