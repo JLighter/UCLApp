cfd88702b7feb3d3aa711b150c828abe65f94d36
Formateur UCL <formateur-ucl@OASIS.UCLOUVAIN.BE>
2019-10-15 13:23:06 +0200

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { VinylModule } from './vinyl/vinyl.module';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     CoreModule,
+    RouterModule.forRoot(routes),
     VinylModule
   ],
   providers: [],
