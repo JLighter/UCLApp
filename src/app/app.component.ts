8e65369d3f73d8bc5d1dddd9730586e9cfc22604
Formateur UCL <formateur-ucl@OASIS.UCLOUVAIN.BE>
2019-10-15 11:54:21 +0200

    Use VINyl service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { VinylService } from './core/services/vinyl.service';
+import { Vinyl } from './core/models/vinyl.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first-app';
 
-  constructor(private vinylService: VinylService) {
-    console.log('use vinyl service in app', vinylService);
+  vinyls: Vinyl[];
+
+  constructor(private vinylService: VinylService) {}
+
+  ngOnInit() {
+    this.vinyls = this.vinylService.getAll();
   }
 
   onClickVinylButton(vinyl) {
