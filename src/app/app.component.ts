51cf995094c433e61f8ae73ed8923831e6d5843c
Formateur UCL <formateur-ucl@OASIS.UCLOUVAIN.BE>
2019-10-15 10:08:11 +0200

    Pipe usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,13 @@ export class AppComponent {
     {
       title: 'Uprising',
       artist: 'Bob Marley',
-      releaseDate: 1980,
+      releaseDate: new Date('June 10, 1980 00:00:00'),
       imageUrl: 'https://static.fnac-static.com/multimedia/Images/FR/NR/4c/d7/12/1234764/1540-1/tsp20161011114552/Uprising.jpg'
     },
     {
       title: 'Thriller',
       artist: 'Michael Jackson',
-      releaseDate: 1980,
+      releaseDate: new Date('November 30, 1982 00:00:00'),
       imageUrl: 'https://i.pinimg.com/originals/47/6d/37/476d37cdb71c6d856ff0d42a65f97b28.jpg'
     }
   ]
